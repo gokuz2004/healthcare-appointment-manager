18e6d23bf984e2896c2028fc05d1707f6718bca0
Gokul Krishna M S <gokulkrishna.ms2023@vitstudent.ac.in>
2026-08-24 22:51:04 +0530

    Add PatientPortal component with AppointmentForm

---
+++ frontend/pages/patient.js
@@ -0,0 +1,10 @@
+import AppointmentForm from "../components/AppointmentForm";
+
+export default function PatientPortal() {
+  return (
+    <div>
+      <h2>Patient Portal</h2>
+      <AppointmentForm />
+    </div>
+  );
+}
