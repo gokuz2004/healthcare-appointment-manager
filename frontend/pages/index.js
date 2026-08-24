9f73c5e89340a8ab8c4ab494b3340787248fc890
Gokul Krishna M S <gokulkrishna.ms2023@vitstudent.ac.in>
2026-08-24 22:49:32 +0530

    Create Home component for Healthcare Appointment Manager

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Healthcare Appointment Manager</h1>
+      <p>Select your portal:</p>
+      <ul>
+        <li><a href="/patient">Patient Portal</a></li>
+        <li><a href="/doctor">Doctor Portal</a></li>
+        <li><a href="/admin">Admin Portal</a></li>
+      </ul>
+    </div>
+  );
+}
