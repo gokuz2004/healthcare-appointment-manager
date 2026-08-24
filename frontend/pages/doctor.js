433f28e9548e96655636e300a197d44ce8736e09
Gokul Krishna M S <gokulkrishna.ms2023@vitstudent.ac.in>
2026-08-24 22:53:00 +0530

    Add DoctorPortal component with AppointmentList

---
+++ frontend/pages/doctor.js
@@ -0,0 +1,10 @@
+import AppointmentList from "../components/AppointmentList";
+
+export default function DoctorPortal() {
+  return (
+    <div>
+      <h2>Doctor Portal</h2>
+      <AppointmentList />
+    </div>
+  );
+}
